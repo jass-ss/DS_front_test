6e30c055f411c01c867b197f3507bb8e64eba7b5
jass--ss <jsmsue010@gmail.com>
2022-08-11 12:07:41 +0900

    style:로그인 모달.

--- component/main/Main.tsx
+++ component/main/Main.tsx
@@ -19,7 +19,6 @@ const Main = (/*{ data }: any*/) => {
 	const ref = useRef<HTMLElement>(null);
 	const context = useContext(ContextUser);
 
-	/*
 	useEffect(() => {
 		if (context.user.job === '') {
 			setOpen(true);
@@ -32,7 +31,7 @@ const Main = (/*{ data }: any*/) => {
 			);
 		}
 	}, []);
-*/
+
 	/*
 	const { data, isLoading, isError } = useQuery(
 		['posts'],
